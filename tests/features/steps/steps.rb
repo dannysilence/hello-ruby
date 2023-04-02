bd2feea2224be476f647252c702ff0e9459082e1
ds <dannysilence@homelabs.company>
2023-04-02 18:01:13 +0300

    ..

--- tests/features/steps/steps.rb
+++ tests/features/steps/steps.rb
@@ -9,36 +9,36 @@ class DummySteps
     self.assertions = 0
     self.failure = nil
   end
-end
 
-Given('this will pass') do
-  @this_will_pass = true
-end
+  Given('this will pass') do
+    @this_will_pass = true
+  end
 
-Given('this will fail') do
-  @this_will_pass = false
-end
+  Given('this will fail') do
+    @this_will_pass = false
+  end
 
-When('I do an action') do
-  
-end
+  When('I do an action') do
+    
+  end
 
-Then('some results should be there') do
-  assert_equal 1, 1
-end
+  Then('some results should be there') do
+    assert_equal 1, 1
+  end
 
-Given('this will pass {int} times') do |n|
-  @a=n
-end
+  Given('this will pass {int} times') do |n|
+    @a=n
+  end
 
-When('I do an action {int} times') do |m|
-  @b=m
-end
+  When('I do an action {int} times') do |m|
+    @b=m
+  end
 
-Then('some results should be there for {int} times') do |k|
-  n = @a
-  m = @b
-  z = n * m
-  @c = z
-  assert_equal(k, z)
+  Then('some results should be there for {int} times') do |k|
+    n = @a
+    m = @b
+    z = n * m
+    @c = z
+    assert_equal(k, z)
+  end
 end
