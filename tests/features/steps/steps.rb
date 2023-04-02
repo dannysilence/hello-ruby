5fd1971300c7dbf307159e782a4ce33e1880b2ea
ds <dannysilence@homelabs.company>
2023-04-02 18:02:41 +0300

    ..

--- tests/features/steps/steps.rb
+++ tests/features/steps/steps.rb
@@ -2,43 +2,43 @@ require 'minitest/autorun'
 
 class DummySteps
   include Minitest::Assertions
-  
+
   attr_accessor :assertions, :failure
-  
+
   def initialize
     self.assertions = 0
     self.failure = nil
   end
+end
 
-  Given('this will pass') do
-    @this_will_pass = true
-  end
+Given('this will pass') do
+  @this_will_pass = true
+end
+  
+Given('this will fail') do
+  @this_will_pass = false
+end
+  
+When('I do an action') do
+  
+end
 
-  Given('this will fail') do
-    @this_will_pass = false
-  end
+Then('some results should be there') do
+  assert_equal 1, 1
+end
 
-  When('I do an action') do
-    
-  end
+Given('this will pass {int} times') do |n|
+  @a=n
+end
 
-  Then('some results should be there') do
-    assert_equal 1, 1
-  end
+When('I do an action {int} times') do |m|
+  @b=m
+end
 
-  Given('this will pass {int} times') do |n|
-    @a=n
-  end
-
-  When('I do an action {int} times') do |m|
-    @b=m
-  end
-
-  Then('some results should be there for {int} times') do |k|
-    n = @a
-    m = @b
-    z = n * m
-    @c = z
-    assert_equal(k, z)
-  end
+Then('some results should be there for {int} times') do |k|
+  n = @a
+  m = @b
+  z = n * m
+  @c = z
+  # assert_equal(k, z)
 end
