efa267153b7912b85e82ef4a95c4778d539ffc7d
ds <dannysilence@homelabs.company>
2023-04-02 17:33:21 +0300

    changed steps.rb to require minitest

--- tests/features/steps/steps.rb
+++ tests/features/steps/steps.rb
@@ -1,38 +1,35 @@
-# features/step_definitions/steps.rb
-require 'test/unit/assertions'
+require 'minitest/autorun'
 
-World(Test::Unit::Assertions)
-
-  Given("this will pass") do
-    @this_will_pass = true
-  end
+Given('this will pass') do
+  @this_will_pass = true
+end
   
-  Given("this will fail") do
-    @this_will_pass = false
-  end
+Given('this will fail') do
+  @this_will_pass = false
+end
   
-  When("I do an action") do
-    
-  end
-
-  Then("some results should be there") do
-    assert_equal 1, 1
-  end
-
-  Given('this will pass {int} times') do |n|
-    @a=n
-  end
-
-  When('I do an action {int} times') do |m|
-    @b=m
-  end
+When('I do an action') do
   
-  Then('some results should be there for {int} times') do |k|
-    n = @a
-    m = @b
-    z = n * m
-    
-    @c = z
-    
-    assert_equal(k, z)
-  end
+end
+
+Then('some results should be there') do
+  assert_equal 1, 1
+end
+
+Given('this will pass {int} times') do |n|
+  @a=n
+end
+
+When('I do an action {int} times') do |m|
+  @b=m
+end
+
+Then('some results should be there for {int} times') do |k|
+  n = @a
+  m = @b
+  z = n * m
+  
+  @c = z
+  
+  assert_equal(k, z)
+end
