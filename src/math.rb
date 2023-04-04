f09ae710286d4d3aa5d44b2eae70d50ff11e018f
dannysilence <you@example.com>
2023-04-04 14:23:15 +0300

    somewhat

---
+++ src/math.rb
@@ -0,0 +1,22 @@
+# math.rb
+class MyMath
+  def self.add # |a|
+    puts a+1
+  end
+
+=begin
+  def self.sub |a, b|
+    puts a-b
+  end
+
+  def self.mul |a, b|
+    puts a*b
+  end
+
+  def self.div |a, b|
+    c=a/b
+    puts c
+    c
+  end
+=end
+end 
---
+++ tests/math_test.rb
@@ -0,0 +1,5 @@
+# math_test.rb
+
+require_relative '../src/math'
+
+
